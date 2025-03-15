61173015c00846eea520b3f5397e64071642b2ec
SabinBaral <sabinbaral7777@gmail.com>
2025-03-15 13:35:08 -0400

    Added UI and up updated socket to let differnt username using RoomName using rooms in socket.io

--- client/src/components/Socket.jsx
+++ client/src/components/Socket.jsx
@@ -1,50 +1,132 @@
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import {
+    Paper,
+    CardHeader,
+    CardContent,
+    Typography,
+    TextField,
+    Button
+} from "@mui/material";
+
 import io from "socket.io-client";
 
 const Socket = () => {
 
-    const [text, setText] = useState("Client not connected");
+    /* Chat Log */
 
-    // useRef is a React Hook that lets you reference a value that’s not needed for rendering
+    const [chatLog, setChatLog] = useState([]);
+
+    // https://react.dev/reference/react/useState#setstate
+    // If you pass a function as nextState, it will be treated as an updater function
+    // It should take the pending state as its only argument and return the next state
+    const appendToChatLog = newLine => setChatLog(currentLog => [...currentLog, newLine]);
+    const renderChatLog = () => chatLog.map((line, index) => <div key={index}>
+        <Typography variant="h6">{line}</Typography>
+    </div>);
+
+    /* Log In */
+
+    // (1) State for room joining data
+    const [userName, setUserName] = useState("");
+    const [joinRoomName, setJoinRoomName] = useState("");
+
+    /* Room */
+
+    const [roomName, setRoomName] = useState("");
+
+    /* WebSocket */
+
+    // https://react.dev/reference/react/useRef
+    // useRef is a React Hook that lets you reference a value that's not needed for rendering
     const effectRan = useRef(false);
+    const socket = useRef();
 
-    const connectToServer = () => 
-    {
-        if (effectRan.current) return; // dont run twice with strict mode 
+    const connectToServer = () => {
+        if (effectRan.current) return; // Don't run twice with Strict Mode
 
-        try{
-            // Later need to be dynamicv for AWS hosting
-
-            const socket = io.connect("localhost:9000",{
+        try {
+            // Only use localhost:9000 if the app is being hosted on port 5173 (i.e. Vite)
+            const wsServerAddress = window.location.port == 5173 ? "localhost:9000" : "/";
+            const ws = io.connect(wsServerAddress, {
                 forceNew: true,
                 transports: ["websocket"],
             });
-            socket.on("connect", () =>
-                setText("Client connected")
-            );
 
-            socket.on("disconnect", () =>
-                setText("Client disconnected")
-            );
+            // (2) Listeners for chat room joining and log updates
+            ws.on("joined", ({ roomName, userName }) => {
+                setRoomName(roomName);
+                appendToChatLog(`${userName} joined room the ${roomName}`);
+            });
 
+            ws.on("user-joined", (message) => {
+                appendToChatLog(message);
+            });
+
+            socket.current = ws;
             effectRan.current = true; // Flag to prevent connecting twice
-
         }
-        catch(e) {
+        catch (e) {
             console.warn(e);
         }
     };
 
+    const joinRoom = () => {
+        // (3) Emit join request to the server
+        if (socket.current && userName && joinRoomName) {
+            socket.current.emit("join", { roomName: joinRoomName, userName });
+        }
+    }
+
+    /* Component Life Cycle */
+
     useEffect(() => {
         connectToServer();
-    },[]);
+    }, []);
 
-    return (
-        <>
-        {text}
-        </>
+    /* Component Rendering - These will become their own components */
+
+    const renderLogInWindow = () => (
+        <Paper elevation={4} sx={{ mt: "1em" }}>
+            <CardContent>
+                <CardHeader title="Join Chat Room" />
+                <TextField fullWidth label="User Name"
+                    sx={{ mb: "1em" }}
+                    value={userName} // (5) Needs to connect with (1)
+                    onChange={(e) => setUserName(e.target.value)} // (5) Needs to connect with (1)
+                />
+                <TextField fullWidth label="Room Name"
+                    sx={{ mb: "1em" }}
+                    value={joinRoomName} // (5) Needs to connect with (1)
+                    onChange={(e) => setJoinRoomName(e.target.value)} // (5) Needs to connect with (1)
+                />
+                
+                {/* Needs to be disabled until both user name and room name exist */}
+                <Button 
+                    fullWidth 
+                    variant="contained" 
+                    disabled={!userName || !joinRoomName}
+                    onClick={() => {
+                        // (6) Needs to connect with (3) to emit the join request
+                        joinRoom();
+                    }}>
+                    Join Room
+                </Button>
+            </CardContent>
+        </Paper >
     );
+
+    const renderChatWindow = () => (
+        <Paper elevation={4} sx={{ mt: "1em" }}>
+            <CardHeader title={roomName} />
+            <CardContent>
+                {renderChatLog()}
+            </CardContent>
+        </Paper>
+    );
+
+    /* App Rendering */
+
+    return roomName ? renderChatWindow() : renderLogInWindow();
 };
 
 export default Socket;
-
