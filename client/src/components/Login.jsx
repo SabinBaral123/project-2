66788b1018c5f59a14ae69ad7d98c4ddb7a0da5b
SabinBaral <sabinbaral7777@gmail.com>
2025-03-22 12:26:19 -0400

    added login to confirm username is not repeated while joining the room

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import {
+    Paper,
+    CardHeader,
+    CardContent,
+    TextField,
+    Button,
+    Alert
+} from "@mui/material";
+
+const Login = (props) => {
+
+    const [roomName, setRoomName] = useState("");
+    const [userName, setUserName] = useState("");
+
+    return (
+        <Paper elevation={4} sx={{ mt: "0.5em" }}>
+            <CardContent>
+                <CardHeader title="Join a Room" />
+                <TextField fullWidth label="User Name" value={userName} onChange={e => setUserName(e.target.value)}
+                    sx={{ mb: "1em" }}
+                />
+                <TextField fullWidth label="Room Name" value={roomName} onChange={e => setRoomName(e.target.value)}
+                    sx={{ mb: "1em" }}
+                />
+                <Button fullWidth variant="contained" disabled={!roomName || !userName}
+                    onClick={() => props.joinRoom({ roomName, userName })}
+                >
+                    Join Room
+                </Button>
+            </CardContent>
+			{props.error && <Alert severity="error">{props.error}</Alert>}
+        </Paper>
+    );
+};
+
+export default Login;
